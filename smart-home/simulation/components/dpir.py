3f7728a1a97ea09c39c861c7499d6c3bc7612260
Bogdan <bogdanjanosevic@gmail.com>
2023-12-07 14:59:33 +0100

    Added mqtt logic for ds and dpir

--- smart-home/simulation/components/dpir.py
+++ smart-home/simulation/components/dpir.py
@@ -2,31 +2,58 @@ import threading
 import time
 from locks.print_lock import print_lock
 
-from simulators.dpir import run_dus_simulator
+from simulators.dpir import run_dpir_simulator
+import paho.mqtt.publish as publish
+from broker_settings import HOSTNAME, PORT
+import json
+dht_batch = []
+publish_data_counter = 0
+publish_data_limit = 5
 
 
-def dpir_callback(motion_detected, code):
-    with print_lock:
-        if motion_detected:
-           t = time.localtime()
-           print("="*20)
-           print(f"Timestamp: {time.strftime('%H:%M:%S', t)}")
-           print(f"Code: {code}")
-           print(f"Motion detected\n")
+def publisher_task(event, pir_batch):
+    global publish_data_counter, publish_data_limit
+    while True:
+        event.wait()
+        with print_lock:
+            local_pir_batch = pir_batch.copy()
+            publish_data_counter = 0
+            pir_batch.clear()
+        publish.multiple(local_pir_batch, hostname=HOSTNAME, port=PORT)
+        print(f'published {publish_data_limit} dpir values')
+        event.clear()
 
+def dpir_callback(motion_detected, settings, publish_event):
+    global publish_data_counter, publish_data_limit
+    if motion_detected:
+        payload = {
+            'measurement': 'Motion',
+            'simulated': settings['simulated'],
+            'runs_on': settings['runs_on'],
+            'name': settings['name'],
+            'value': 1,
+        }
+        with print_lock:
+            dht_batch.append(('dpir', json.dumps(payload), 0, True))
+            publish_data_counter += 1
+        if publish_data_counter >= publish_data_limit:
+            publish_event.set()
 
-def run_dpir(settings, threads, stop_event, code):
+publish_event = threading.Event()
+publisher_thread = threading.Thread(target=publisher_task, args=(publish_event, dht_batch,))
+publisher_thread.daemon = True
+publisher_thread.start()
+
+def run_dpir(settings, threads, stop_event):
         if settings['simulated']:
-            print("Starting " + code + " sumilator")
-            dpir_thread = threading.Thread(target = run_dus_simulator, args=(5, dpir_callback, stop_event, code))
+            dpir_thread = threading.Thread(target=run_dpir_simulator,
+                                          args=(5, dpir_callback, stop_event, settings, publish_event))
             dpir_thread.start()
             threads.append(dpir_thread)
-            print(code + " sumilator started\n")
         else:
             from sensors.pir import detect_motion
-            print("Starting " + code + " loop")
             pin = settings['pin']
-            pir_thread = threading.Thread(target=detect_motion, args=(pin, code))
-            pir_thread.start()
-            threads.append(pir_thread)
-            print(code + " loop started")
+            dpir_thread = threading.Thread(target=detect_motion,
+                                          args=(pin, dpir_callback, stop_event, settings, publish_event))
+            dpir_thread.start()
+            threads.append(dpir_thread)
