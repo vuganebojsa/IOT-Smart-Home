8da9676294d4b2e2cd72c2e9e519f68fac02e276
Nebojša Vuga <vuga.sv53.2020@uns.ac.rs>
2023-12-28 14:27:20 +0100

    Added bir

---
+++ smart-home/simulation/components/bir.py
@@ -0,0 +1,66 @@
+import threading
+import time
+from locks.print_lock import print_lock
+from datetime import datetime, timedelta
+
+from simulators.bir import run_bir_simulator
+import paho.mqtt.publish as publish
+from broker_settings import HOSTNAME, PORT
+import json
+bir_batch = []
+publish_data_counter = 0
+publish_data_limit = 5
+
+
+def publisher_task(event, bir_batch):
+    global publish_data_counter, publish_data_limit
+    while True:
+        event.wait()
+        with print_lock:
+            local_pir_batch = bir_batch.copy()
+            publish_data_counter = 0
+            bir_batch.clear()
+        publish.multiple(local_pir_batch, hostname=HOSTNAME, port=PORT)
+        print(f'published {publish_data_limit} bir values')
+        event.clear()
+
+def bir_callback(motion_detected, settings, publish_event):
+    global publish_data_counter, publish_data_limit
+    if motion_detected:
+        current_datetime = datetime.now()
+
+        adjusted_datetime = current_datetime - timedelta(hours=1)
+
+        formatted_time = adjusted_datetime.isoformat()
+        payload = {
+            'measurement': 'Motion',
+            'simulated': settings['simulated'],
+            'runs_on': settings['runs_on'],
+            'name': settings['name'],
+            'value': 1,
+            '_time': formatted_time
+        }
+        with print_lock:
+            bir_batch.append(('bir', json.dumps(payload), 0, True))
+            publish_data_counter += 1
+        if publish_data_counter >= publish_data_limit:
+            publish_event.set()
+
+publish_event = threading.Event()
+publisher_thread = threading.Thread(target=publisher_task, args=(publish_event, bir_batch,))
+publisher_thread.daemon = True
+publisher_thread.start()
+
+def run_dpir(settings, threads, stop_event):
+        if settings['simulated']:
+            dpir_thread = threading.Thread(target=run_bir_simulator,
+                                          args=(5, bir_callback, stop_event, settings, publish_event))
+            dpir_thread.start()
+            threads.append(dpir_thread)
+        else:
+            from sensors.pir import detect_motion
+            pin = settings['pin']
+            dpir_thread = threading.Thread(target=detect_motion,
+                                          args=(pin, bir_callback, stop_event, settings, publish_event))
+            dpir_thread.start()
+            threads.append(dpir_thread)
--- smart-home/simulation/pi3.py
+++ smart-home/simulation/pi3.py
@@ -3,12 +3,7 @@ import threading
 from settings import load_settings
 from components.dht import run_dht
 from components.pir import run_pir
-from components.dus import run_dus
 import time
-from components.dpir import run_dpir
-from components.ds import run_ds
-from components.dms import run_dms
-from components.dl import run_dl
 from components.db import run_db
 from locks.print_lock import print_lock
 
@@ -20,41 +15,21 @@ except:
 
 
 def run_dht_threads(settings, threads, stop_event):
-    rdht1_settings = settings['RDHT1']
-    rdht2_settings = settings['RDHT2']
-    run_dht(rdht1_settings, threads, stop_event, 'RDHT1')
-    run_dht(rdht2_settings, threads, stop_event, 'RDHT2')
+    rdht4_settings = settings['RDHT4']
+    run_dht(rdht4_settings, threads, stop_event, 'RDHT4')
 
 def run_pir_threads(settings, threads, stop_event):
-    rpir1_settings = settings['RPIR1']
-    rpir2_settings = settings['RPIR2']
-    run_pir(rpir1_settings, threads, stop_event)
-    run_pir(rpir2_settings, threads, stop_event)
+    rpir4_settings = settings['RPIR4']
+    run_pir(rpir4_settings, threads, stop_event)
 
-def run_dpir_threads(settings, threads, stop_event):
-    dpir1_settings = settings['DPIR1']
+def run_bir_threads(settings, threads, stop_event):
+    bir_settings = settings['BIR']
 
-    run_dpir(dpir1_settings, threads, stop_event)
+    #run_dpir(bir_settings, threads, stop_event)
 
-def run_ds_threads(settings, threads, stop_event):
-    ds1_settings = settings['DS1']
-    run_ds(ds1_settings, threads, stop_event, 'DS1')
-
-def run_dus_threads(settings, threads, stop_event):
-    dus1_settings = settings['DUS1']
-    run_dus(dus1_settings, threads, stop_event)
-
-def run_dms_threads(settings, threads, stop_event):
-    dms_settings = settings['DMS']
-    run_dms(dms_settings, threads, stop_event)
-
-def run_dl_threads(settings, threads, stop_event):
-    dl_settings = settings["DL"]
-    run_dl(dl_settings, threads, stop_event, "DL")
-
-def run_db_threads(settings, threads, stop_event):
-    db_settings = settings["DB"]
-    run_db(db_settings, threads, stop_event, "DB")
+def run_bb_threads(settings, threads, stop_event):
+    db_settings = settings["BB"]
+    run_db(db_settings, threads, stop_event, "BB")
 
 
 
@@ -67,14 +42,14 @@ def menu(stop_event):
                 with print_lock:
                     print("Menu Options:")
                     print("Press l to control Door Light")
-                    print("Press b to control Door Buzzer")
+                    print("Press b to control Bedroom Buzzer")
                     print("Press 'e' to exit the menu")
                     user_input = input("Enter your choice: ")
                     if user_input == "l":
-                        run_dl_threads(settings, threads, stop_event)
+                        #run_dl_threads(settings, threads, stop_event)
                         time.sleep(1)
                     elif user_input == "b":
-                        run_db_threads(settings, threads, stop_event)
+                        run_bb_threads(settings, threads, stop_event)
                         time.sleep(1)
                     elif user_input == "e":
                         print("Exiting the menu. Printing is resumed.")
@@ -95,10 +70,7 @@ if __name__ == "__main__":
     try:
         run_dht_threads(settings, threads, stop_event)
         run_pir_threads(settings, threads, stop_event)
-        run_dpir_threads(settings, threads, stop_event)
-        run_ds_threads(settings, threads, stop_event)
-        run_dus_threads(settings, threads, stop_event)
-        run_dms_threads(settings, threads, stop_event)
+        run_bir_threads(settings, threads, stop_event)
         run_menu_thread(threads, stop_event)
         while True:
             
