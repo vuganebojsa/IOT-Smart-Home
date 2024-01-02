39a40dee920f897b8c573e36bf8261f9b907a21e
Nebojša Vuga <vuga.sv53.2020@uns.ac.rs>
2024-01-02 09:34:19 +0100

    deleted rn menu

--- smart-home/simulation/pi1.py
+++ smart-home/simulation/pi1.py
@@ -99,7 +99,7 @@ if __name__ == "__main__":
         run_ds_threads(settings, threads, stop_event)
         run_dus_threads(settings, threads, stop_event)
         run_dms_threads(settings, threads, stop_event)
-        run_menu_thread(threads, stop_event)
+        #run_menu_thread(threads, stop_event)
         while True:
             
             time.sleep(0.1)
--- smart-home/simulation/pi3.py
+++ smart-home/simulation/pi3.py
@@ -33,35 +33,6 @@ def run_bb_threads(settings, threads, stop_event):
     run_db(db_settings, threads, stop_event, "BB")
 
 
-
-def menu(stop_event):
-    while not stop_event.is_set():
-        user_input = input("Press 'm' to open the menu: ")
-        print("user input: ", user_input)
-        if user_input == "m":
-            while True:
-                with print_lock:
-                    print("Menu Options:")
-                    print("Press l to control Door Light")
-                    print("Press b to control Bedroom Buzzer")
-                    print("Press 'e' to exit the menu")
-                    user_input = input("Enter your choice: ")
-                    if user_input == "l":
-                        #run_dl_threads(settings, threads, stop_event)
-                        time.sleep(1)
-                    elif user_input == "b":
-                        run_bb_threads(settings, threads, stop_event)
-                        time.sleep(1)
-                    elif user_input == "e":
-                        print("Exiting the menu. Printing is resumed.")
-                        break
-        else:
-            pass
-def run_menu_thread(threads, stop_event):
-    thread = threading.Thread(target = menu, args=(stop_event,))
-    thread.start()
-    threads.append(thread)
-
 if __name__ == "__main__":
     print('Starting app')
     settings = load_settings('settingspi3.json')
@@ -72,9 +43,7 @@ if __name__ == "__main__":
         run_dht_threads(settings, threads, stop_event)
         run_pir_threads(settings, threads, stop_event)
         run_bir_threads(settings, threads, stop_event)
-        run_menu_thread(threads, stop_event)
         while True:
-            
             time.sleep(0.1)
     except KeyboardInterrupt:
         print('Stopping app')
