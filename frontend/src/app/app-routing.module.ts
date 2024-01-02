be93833816656c68e5ed80eff384fa8bd30ce755
Nebojša Vuga <vuga.sv53.2020@uns.ac.rs>
2024-01-02 22:27:40 +0100

    added navigation

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AlarmClockComponent } from './components/alarm-clock/alarm-clock.component';
+import { HouseAlarmComponent } from './components/house-alarm/house-alarm.component';
+import { HomeComponent } from './components/home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'alarm-clock', component: AlarmClockComponent},
+  {path: 'house-alarm', component: HouseAlarmComponent},
+  { path: '', redirectTo: '', pathMatch: 'full'},
+  { path: '**', component: HomeComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
