d3b7b367cce4e7b5a6957bfd691008264ea745aa
Nebojša Vuga <vuga.sv53.2020@uns.ac.rs>
2024-01-05 12:17:13 +0100

    added something

--- frontend/src/app/components/home/home.component.ts
+++ frontend/src/app/components/home/home.component.ts
@@ -1,14 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { Socket } from 'ngx-socket-io';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent {
-  constructor(private route: Router){}
+export class HomeComponent implements OnInit, OnDestroy{
+  constructor(private route: Router, private socket: Socket){}
+  alarmStatus:any;
+  ngOnDestroy(): void {
+  }
+  ngOnInit(): void {
+
+  }
   showSensor(sensorName:string){
     this.route.navigate(['device/' + sensorName])
   }
+
+
 }
